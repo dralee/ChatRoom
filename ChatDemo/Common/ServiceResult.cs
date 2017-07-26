1b0e7caa0b957b2e3974ddb825962c2a4ffb44a7
dralee <zhzl_s@126.com>
2017-07-26 09:38:12 +0800

    initialize the chatroom project

---
+++ ChatDemo/Common/ServiceResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class ServiceResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+}
